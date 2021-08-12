432744ab2b2a58696a9bd0c1e183a28f7ffe1de4
reve23 <reve172020@gmail.com>
2021-08-12 23:10:15 +0600

    probably work on this someday

---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Main() {
+    return (
+        <div className="text-center">
+            <h1 className="text-center">Welcome To Misconception Of Programming</h1>
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="..." class="d-block w-100" alt="one"/>
+                        </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="two"/>
+                        </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="three"/>
+                        </div>
+                </div>
+            </div>
+        </div>
+    )
+}
